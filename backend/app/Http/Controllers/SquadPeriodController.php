c248f848f9af3b9e43b324509c2b49a6ad78476c
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-12-08 18:33:39 -0300

    feat: create squad period

---
+++ backend/app/Http/Controllers/SquadPeriodController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSquadPeriodRequest;
+use App\Http\Requests\UpdateSquadPeriodRequest;
+use App\Models\SquadPeriod;
+
+class SquadPeriodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSquadPeriodRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SquadPeriod $squadPeriod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SquadPeriod $squadPeriod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSquadPeriodRequest $request, SquadPeriod $squadPeriod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SquadPeriod $squadPeriod)
+    {
+        //
+    }
+}
